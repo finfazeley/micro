bdec1cc04fccf68010a7e223702e9130f7abab8b
alookasank <alookasank@myvuw.ac.nz>
2023-09-30 20:40:23 +1300

    Added auth page

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,10 @@ router.get('/', (req, res, next) => {
   res.render('index');
 })
 
+router.get('/auth', (req, res, next) => {
+  res.render('auth');
+})
+
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 
