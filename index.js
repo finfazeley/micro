b95585829fba4b8770b746f7c6e51a5e62436478
Sankeerth Alookaran Jivan <alookasank@myvuw.ac.nz>
2023-10-01 00:00:29 +1300

    Fix authentication

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use('/', routes);
 app.set('view engine', 'ejs');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/TradeCarsDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://0.0.0.0:27017/TradeCarsDB', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
