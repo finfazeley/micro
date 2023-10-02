d38ba5851832bd0ff4884f8dd07e85bb1b2abc1a
Sankeerth Alookaran Jivan <alookasank@myvuw.ac.nz>
2023-10-02 23:56:32 +1300

    Homepage gets all listings from database

--- controllers/ListingController.js
+++ controllers/ListingController.js
@@ -34,3 +34,25 @@ exports.getSellPage = (req, res, next) => {
     });
   });
 }
+
+// Utility Functions
+
+exports.getAllListings = async () => {
+  const listings = await CarListing.find().cursor().toArray();
+  var newListings = [];
+  for await (const list of listings) {
+    await User.findById(list.user.toHexString()).then(res => {
+      newList = {
+        user: res.username,
+        make: list.make,
+        model: list.model,
+        year: list.year,
+        mileage: list.mileage,
+        description: list.description,
+        price: list.price
+      }
+      newListings.push(newList);
+    });
+  }
+  return newListings;
+}
