c6176af66bf6659abd30bd61c80fb7beb887ab0b
Sankeerth Alookaran Jivan <alookasank@myvuw.ac.nz>
2023-10-18 19:10:32 +1300

    Remove index.html to trigger rewrite on host url

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,17 @@
+// Firebase
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-
+// Dependencies
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const cookies = require('cookie-parser');
-
+const passport = require('passport');
+// Local
 const routes = require('./routes/routes');
 const googlePassport = require('./config/google_passport');
-const passport = require('passport');
 
 const app = express();
 
@@ -30,15 +31,12 @@ app.use(passport.session());
 /**
  * Serve static files in public directory
  */
-app.use(express.static(path.join(__dirname, 'public/styles')));
 app.use(express.urlencoded({ extended:false }));
 
 // To parse JSON data
 app.use(express.json());
 
-
 app.use('/', routes);
-
 
 /**
  * Set template engine to ejs
@@ -49,22 +47,5 @@ app.set('view engine', 'ejs');
 mongoose.connect('mongodb://0.0.0.0:27017/TradeCarsDB', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
-
-// app.get('/protected', verifyToken, (req, res) => {
-//   res.send('This is a protected route!');
-// });
-
-//app.use((req, res) => { res.send("Page Not Found")});
-
-// app.listen(5001, () => {
-//   console.log("Heard on 5001");
-// }).on('error', (err) => {
-//   // check if the error thrown is an address in use error
-//   if (err.errno === 'EADDRINUSE') {
-//     console.log("Port 5001 is already in use");
-//   } else {
-//     console.log(err);
-//   }
-// });
 
 exports.app = onRequest(app);
