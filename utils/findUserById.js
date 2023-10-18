6e2cf86ea3742ffa500bfcf1d2b632f03738eba9
Sankeerth Alookaran Jivan <alookasank@myvuw.ac.nz>
2023-10-18 20:23:17 +1300

    Fix merge issues

--- utils/findUserById.js
+++
@@ -1,7 +0,0 @@
-const User = require('../models/User');
-
-module.exports = async(userID) => {
-  if (!userID || userID === undefined) {return null;}
-  const user = await User.findById(userID);
-  return user.username;
-}
