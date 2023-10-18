179c993fe68f9fd05df2e0a69f10615d2fc0a154
illfinley <finley@ill-intentions.com>
2023-10-18 14:31:58 +1300

    updating app with stuff from main

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-//const functions = require('firebase-functions');
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
@@ -65,23 +56,15 @@ mongoose.connect('mongodb://0.0.0.0:27017/TradeCarsDB', { useNewUrlParser: true,
 
 //app.use((req, res) => { res.send("Page Not Found")});
 
-app.listen(5001, () => {
-  console.log("Heard on 5001");
-}).on('error', (err) => {
-  // check if the error thrown is an address in use error
-  if (err.errno === 'EADDRINUSE') {
-    console.log("Port 5001 is already in use");
-  } else {
-    console.log(err);
-  }
-});
+// app.listen(5001, () => {
+//   console.log("Heard on 5001");
+// }).on('error', (err) => {
+//   // check if the error thrown is an address in use error
+//   if (err.errno === 'EADDRINUSE') {
+//     console.log("Port 5001 is already in use");
+//   } else {
+//     console.log(err);
+//   }
+// });
 
 exports.app = onRequest(app);
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
