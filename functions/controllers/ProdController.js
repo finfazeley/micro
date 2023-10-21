1c9e2a63934b88b9030371d372d0f333caafdc83
illfinley <finley@ill-intentions.com>
2023-10-21 21:24:30 +1300

    entering correct details deletes item from mongodb

--- functions/controllers/ProdController.js
+++ functions/controllers/ProdController.js
@@ -49,20 +49,37 @@ exports.completePurchase = async (req, res) => {
 
         if(!product) {
             console.log("No product found");
-            res.status(500).json({message: 'No product found'});return;
+            res.status(400).json({message: 'No product found'});
+            return;
         }
         // check user credentials match
+        if (req.body.username !== req.body.userName){
+            res.status(400).json({message: 'Incorrect email'});         
+            return;
+        }
+        
 
-
-        else {
-            console.log("Product found");
-            console.log(req.body);
-
-            // delete the car from mongodb
-            
+        // "purchase" the car from mongodb
+        const result = await CarListing.deleteOne({_id: req.body.prod});
+        if (result.deletedCount === 1) {
+            console.log("Successfully deleted one document.");
+          } else {
+            console.log("No documents matched the query. Deleted 0 documents.");
         }
 
+        // finished
         return res.redirect('/');
+
+
+        // else {
+        //     console.log("Product found");
+        //     console.log(req.body);
+
+        //     // delete the car from mongodb
+            
+        // }
+
+        
     } catch (err) {
       res.status(500).json({message: err.message});
     }
