b3822c105ba1357f9efa7d47ddc91a9c40b612e5
Sankeerth Alookaran Jivan <alookasank@myvuw.ac.nz>
2023-10-21 18:18:20 +1300

    Update navPages

--- functions/utils/navPages.js
+++ functions/utils/navPages.js
@@ -1,10 +1,10 @@
 const navPages = [
   { name: 'Home', url: '/', onLogOut: true, onLogIn: true },
   { name: 'Sell', url: '/sell', onLogOut: false, onLogIn: true},
-  { name: 'Register', url: '/register', onLogOut: false, onLogIn: false},
-  { name: 'Login', url: '/auth', onLogOut: false, onLogIn: false},
+  { name: 'Register', url: '/register', onLogOut: true, onLogIn: false},
   { name: 'My Listings', url: '/myListings', onLogOut: false, onLogIn: true},
-  { name: 'Account', url: '/account', onLogOut: false, onLogIn: true}
+  { name: 'Account', url: '/account', onLogOut: false, onLogIn: true},
+  { name: 'Login', url: '/auth', onLogOut: true, onLogIn: false}
 ]
 
 export default navPages;
