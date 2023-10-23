0a65bcf6d7d92dfe9584018a657aa1cabab55ed3
illfinley <finley@ill-intentions.com>
2023-10-23 18:51:56 +1300

    works

--- searchsort/sorter.js
+++ searchsort/sorter.js
@@ -1,9 +1,9 @@
 const CarListing = require("./schemas/CarListing");
 
 exports.sortByYear = async(req, res) => {
-  console.log(CarListing.find());
+  // console.log(CarListing.find());
   const order = (req.query.order === "asc") ? 1 : -1;
-  console.log(order);
+  // console.log(order);
 
   const cursor = CarListing.find().cursor();
   const listings = [];
@@ -22,10 +22,14 @@ exports.sortByYear = async(req, res) => {
 }
 
 exports.sortByPrice = async(req, res) => {
-  console.log(CarListing.find());
   const order = (req.query.order === "asc") ? 1 : -1;
-  console.log(order);
-  var listings = await CarListing.find().cursor().toArray();
+  const cursor = CarListing.find().cursor();
+  const listings = [];
+  for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+    listings.push(doc);
+  }
+
+  //var listings = await CarListing.find().cursor().toArray();
   listings.sort((a, b) => {
     if (a.price > b.price) {
       return 1*order;
@@ -37,10 +41,13 @@ exports.sortByPrice = async(req, res) => {
 }
 
 exports.sortByMileage = async(req, res) => {
-  console.log(CarListing.find());
   const order = (req.query.order === "asc") ? 1 : -1;
-  console.log(order);
-  var listings = await CarListing.find().cursor().toArray();
+  const cursor = CarListing.find().cursor();
+  const listings = [];
+  for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+    listings.push(doc);
+  }
+  //var listings = await CarListing.find().cursor().toArray();
   listings.sort((a, b) => {
     if (a.mileage > b.mileage) {
       return 1*order;
